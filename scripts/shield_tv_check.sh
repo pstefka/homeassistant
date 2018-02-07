eeb6131b187d9836b1229ff6666c5e85eb737e49
Peter Stefka <www.stefi@gmail.com>
2018-02-07 21:36:15 +0000

    add automation for thermostat + shield on/off switch, kodi integration, etc

---
+++ scripts/shield_tv_check.sh
@@ -0,0 +1,41 @@
+#!/bin/bash
+
+export PATH="/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin"
+
+ip_check=$1
+debug=$2
+
+if [ ! $ip_check ]; then
+  # TV IP
+  ip_check='192.168.1.4'
+fi
+
+if [ "$debug" ]; then
+  date
+  echo $ip_check
+fi
+
+if [ "$debug" ]; then
+  adb connect $ip_check
+  sleep 1
+  adb shell dumpsys power | grep 'mWakefulness=Awake'
+  # Awake | Asleep
+else
+  adb connect $ip_check &> /dev/null
+  sleep 1
+  adb shell dumpsys power | grep 'mWakefulness=Awake' &> /dev/null
+fi
+
+if [ $? -eq 0 ]; then
+  echo OK
+else
+  echo FAIL
+fi
+
+if [ "$debug" ]; then
+  echo "----------------------"
+fi
+
+adb disconnect &> /dev/null
+
+
